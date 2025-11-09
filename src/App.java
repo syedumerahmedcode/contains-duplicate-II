59c664bff7931f8db2f7de0397c525af929c821a
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-11-09 22:00:12 +0100

    Feature: Implemented logic of the solution.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        /**
+         * Contains duplicate II
+         * 
+         * Given an integer arraqy ums and an integer k, return true if there are two
+         * distinct indices i and j in the array such that nums[i]=nums[j] and abs (i-j)
+         * <=k
+         */
         System.out.println("Hello, World!");
     }
 }
