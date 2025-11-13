b034bf72905f4500f41a36a59eee3c657a996732
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2025-11-13 13:45:42 +0100

    Feature: Added comments and used the code logic in the main method.

--- src/App.java
+++ src/App.java
@@ -1,12 +1,20 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
         /**
          * Contains duplicate II
          * 
-         * Given an integer arraqy ums and an integer k, return true if there are two
+         * Given an integer array nums and an integer k, return true if there are two
          * distinct indices i and j in the array such that nums[i]=nums[j] and abs (i-j)
          * <=k
          */
+        int[] nums = { 0, 5, 4, 2, 5, 1, 6 };
+        int k = 3;
         System.out.println("Hello, World!");
+        Solution solution = new Solution();
+        boolean containsDuplicatesNearby = solution.containsNearbyDuplicate(nums, k);
+        System.out.println("Does the given input string " + Arrays.toString(nums) + " contains dupiactes nearby:"
+                + containsDuplicatesNearby);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -3,13 +3,23 @@ import java.util.Set;
 
 public class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+        /**
+         * We have the additional data structure, HashSet to save numbers that have
+         * already been read in a sliding window
+         */
         Set<Integer> set = new HashSet<>();
+        // We loop through the numbers
         for (int i = 0; i < nums.length; ++i) {
+            // Checking the condition: nums[i]=nums[j]
             if (set.contains(nums[i])) {
                 return true;
             }
+            // if the number is not present already in the hashset, insert it in
             set.add(nums[i]);
+            // If the size of the hashset is greater than k,. as in greater tha the sliding
+            // window allowed.
             if (set.size() > k) {
+                // remove the most oldest element inserted ouside of the sliding window.
                 set.remove(nums[i - k]);
             }
         }
